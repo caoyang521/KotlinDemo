7795e639aed95cd514a648cec07f7fedb1fcb641
Mr_阿汤哥 <981319008@qq.com>
2016-03-16 13:54:59 +0800

    2

--- app/src/main/java/com/atg/kotdemo/MainActivity.kt
+++ app/src/main/java/com/atg/kotdemo/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
+/**
+ * Kotlin first
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        text.text="Hello Kot";
+        text.text = "Hello Kot";
+        text.textSize = 15f;
 
     }
 }
